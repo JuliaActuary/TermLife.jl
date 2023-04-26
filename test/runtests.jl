04c9dd9855ee1b8d7ce3d6e10bc6d47172b48711
MatthewCaseres <matthewcaseres@outlook.com>
2023-04-25 19:19:37 -0500

    add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
-using TermLife
+using TermLife: TermLife as tl
 using Test
 
 @testset "TermLife.jl" begin
+    @test round(tl.glm_prediction_test()[1], digits=3) == .912
     # Write your tests here.
 end
