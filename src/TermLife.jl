afa25ded5a64b38df06da9b86a86951707d41287
MatthewCaseres <matthewcaseres@outlook.com>
2023-04-25 19:19:40 -0500

    add test

--- src/TermLife.jl
+++ src/TermLife.jl
@@ -1,5 +1,14 @@
 module TermLife
+using Dates
 
 include("encodings.jl")
+
+
+struct State
+    from::Date
+    to::Date
+    policy_timestep::Int
+    mortality_table_index::Int
+end
 
 end # module TermLife
