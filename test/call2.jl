c728ffe8f978661b58518e54bb066c28623cc02b
MatthewCaseres <matthewcaseres@outlook.com>
2023-04-28 20:35:26 -0500

    maybe quicker

---
+++ test/call2.jl
@@ -0,0 +1,14 @@
+function get_mortality_over_range(from::Date, to::Date, rate::Float64)::Float64
+    1 - (1 - rate)^((Dates.value(to - from) + 1)/365.25)
+end
+
+function simulate()
+    month_ranges = get_month_ranges(Date(2020,1,1), Date(2020,12,31))
+    for (from, to, timestep) in month_ranges
+        duration = (timestep - 1) ÷ 12
+        if rand() < get_mortality_over_range(from, to, .1)
+            return 1
+        end
+    end
+    return 0
+end
